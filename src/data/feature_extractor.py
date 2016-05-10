80b7b4da2bb4c86120cf68f28dbd572610f9ec5a
babytuan <ouyangshuxin@gmail.com>
2016-05-10 21:37:48 +0800

    add historical popularity features

--- src/data/feature_extractor.py
+++ src/data/feature_extractor.py
@@ -13,8 +13,8 @@ from datetime import timedelta
 # extract features for videos published on certain date
 # video features
 # user features
-# topic features : tag count, des length, title
-# text features: # u-name, u-description, v-title 
+# topic features
+# text features 
 # history features
 
 # for each level of vc30, extract the tags
@@ -625,214 +625,85 @@ def get_video_info(vci_file, sourcename_file, tag_path_prefix, titleword_path_pr
     
     
     
+# vci1-7, vci_rate1-7, burst, vc, comment, favorite, up, down
+def get_historical_populairty(vci_file, json_path, date_str, historical_popularity_file):
+    # get <vid, vci> for one-day video set
+    vid_vci_map = {}
+    vci_fd = open(vci_file, 'r')
+    for line in vci_fd.readlines():
+        fields = line.strip().split('\t', -1)
+        # vid, vci1, vci2, ..., vci30
+        vid_vci_map[fields[0]] = []
+        for i in range(1, 1 + 7):
+            vid_vci_map[fields[0]].append(int(fields[i]))
+    vci_fd.close()
+    
+    # for the last observation date
+    first_date = date(int(date_str[0 : 4]), int(date_str[5 : 7]), int(date_str[8 : 10]))
+    day_delta = timedelta(days = 6)
+    last_date = first_date + day_delta
+    json_fd = open(json_path + str(first_date) + '_' + str(last_date), 'r')
+    historical_popularity_fd = open(historical_popularity_file, 'w')
+    for line in json_fd.readlines():
+        video_metadata = json.loads(line.strip())
+        if video_metadata['id'] in vid_vci_map.keys():
+            historical_popularity_fd.write(video_metadata['id'])
+            for vci in vid_vci_map[video_metadata['id']]:
+                historical_popularity_fd.write('\t%d' % vci)
+            s = sum(vid_vci_map[video_metadata['id']])
+            burst_flag = False
+            for vci in vid_vci_map[video_metadata['id']]:
+                if 0 == s:
+                    historical_popularity_fd.write('\t%0')
+                else:
+                    historical_popularity_fd.write('\t%0.4f' % (1. * vci / s))
+                if 0 < s and 3 * 1. / 7 <= 1. * vci / s:
+                    burst_flag = True
+            historical_popularity_fd.write('\t' + str(burst_flag))
+            historical_popularity_fd.write('\t%d\t%d\t%d\t%d\t%d' % 
+                                           (video_metadata['view_count'], 
+                                            int(video_metadata['comment_count']), 
+                                            int(video_metadata['favorite_count']), 
+                                            int(video_metadata['up_count']), 
+                                            int(video_metadata['down_count'])))
+            historical_popularity_fd.write('\n')
+            
+            vid_vci_map.pop(video_metadata['id'])
+            
+    json_fd.close()
+     
+    # for the second to the last observation date
+    day_delta = timedelta(days = 5)
+    last_date = first_date + day_delta
+    json_fd = open(json_path + str(first_date) + '_' + str(last_date), 'r')
+    for line in json_fd.readlines():
+        video_metadata = json.loads(line.strip())
+        if video_metadata['id'] in vid_vci_map.keys():
+            historical_popularity_fd.write(video_metadata['id'])
+            for vci in vid_vci_map[video_metadata['id']]:
+                historical_popularity_fd.write('\t%d' % vci)
+            s = sum(vid_vci_map[video_metadata['id']])
+            burst_flag = False
+            for vci in vid_vci_map[video_metadata['id']]:
+                if 0 == s:
+                    historical_popularity_fd.write('\t%0')
+                else:
+                    historical_popularity_fd.write('\t%0.4f' % (1. * vci / s))
+                if 0 < s and 3 * 1. / 7 <= 1. * vci / s:
+                    burst_flag = True
+            historical_popularity_fd.write('\t' + str(burst_flag))
+            historical_popularity_fd.write('\t%d\t%d\t%d\t%d\t%d' % 
+                                           (video_metadata['view_count'], 
+                                            int(video_metadata['comment_count']), 
+                                            int(video_metadata['favorite_count']), 
+                                            int(video_metadata['up_count']), 
+                                            int(video_metadata['down_count'])))
+            historical_popularity_fd.write('\n')
+            
+    json_fd.close()
+    historical_popularity_fd.close()
     
     
-    
-    
-# 
-# def extractFeatures(videofile, userfile, labelfile, outfile, indidate, details = False):
-#     userMap = getUserFeatures(userfile)
-#     videoMap = getVideoFeatures(videofile)
-#     labelFd = open(labelfile, 'r')
-#     outFd = open(outfile, 'w')
-#     header = 'vid\tn30\tlabel\ttitle_len\ttitle_cnchar_num\ttitle_cnchar_rate\ttitle_noncnchar_num\ttitle_noncnchar_rate\t\
-# title_digit_num\ttitle_digit_rate\ttitle_letter_num\ttitle_letter_rate\ttitle_space_num\ttitle_space_rate\t\
-# title_booktitle_flag\tvideo_des_len\tduration\tcategory\tpublic_type\ttag_num\tcopyright_type\tstreamtype_num\t\
-# streamtypes_hd2\tuser_len\tuser_cnchar_num\tuser_cnchar_rate\tuser_noncnchar_num\tuser_noncnchar_rate\t\
-# user_digit_num\tuser_digit_rate\tuser_letter_num\tuser_letter_rate\tgender\tuser_des_len\tvideos_count\t\
-# playlists_count\tfavorites_count\tfollowers_count\tfollowing_count\tstatuses_count\tsubscribe_count\tvv_count\t\
-# is_vip\tis_share\tis_verified'
-#     for i in range(1, 1 + indidate):
-#         header = header + '\ti' + str(i)
-#     header = header + '\tn' + str(indidate)
-#     for i in range(1, 1 + indidate):
-#         header = header + '\ti' + str(i) + '_pct'
-#     outFd.write(header + '\n')
-#     for line in labelFd.readlines():
-#         fields = line.strip().split('\t', -1)
-#         # vid, i1, i2, ..., i30, label
-#         vid = fields[0]
-#         label = fields[31]
-#         if vid in videoMap:
-#             uid = videoMap[vid][0]
-#             if uid in userMap:
-#                 vciList = []
-#                 for i in range(1, 1 + 30):
-#                     vciList.append(int(fields[i]))
-#                 # extract features
-#                 features = []
-#                 # 1:title, description, duration, category, public_type, tags, copyright_type, streamtypes
-#                 # title length
-#                 titleLen = 0 if isinstance(videoMap[vid][1], NoneType) else len(videoMap[vid][1])
-#                 features.append(titleLen)
-#                 # ---regular expression---
-#                 
-#                 digitPart = re.compile('\d')
-#                 letterPart = re.compile('[A-Za-z_-]')
-#                 spacePart = re.compile(r' ')
-#                 booktitlePart = re.compile(ur'《.*》')
-#                 if 0 == titleLen:
-#                     # cn char num in title
-#                     features.append(0)
-#                     # cn char rage in title
-#                     features.append(0)
-#                     # non-cn char num in title
-#                     features.append(0)
-#                     # non-cn char rate in title
-#                     features.append(0)
-#                     # digit num in title
-#                     features.append(0)
-#                     # dig rate in title
-#                     features.append(0)
-#                     # letter num in title
-#                     features.append(0)
-#                     # letter rate in title
-#                     features.append(0)
-#                     # space num in title
-#                     features.append(0)
-#                     # space rate in title
-#                     features.append(0)
-#                     # booktitle flag in title
-#                     features.append(False)
-#                 else:
-#                     
-#                     digitLen = len(digitPart.findall(videoMap[vid][1]))
-#                     letterLen = len(letterPart.findall(videoMap[vid][1]))
-#                     spaceLen = len(spacePart.findall(videoMap[vid][1]))
-#                     booktitleFlag = True if 0 < len(booktitlePart.findall(videoMap[vid][1])) else False
-#                     # cn char num in title
-#                     features.append(cncharLen)
-#                     # cn char rage in title
-#                     features.append(cncharLen * 1. / titleLen)
-#                     # non-cn char num in title
-#                     features.append(titleLen - cncharLen)
-#                     # non-cn char rate in title
-#                     features.append((titleLen - cncharLen) * 1. / titleLen)
-#                     # digit num in title
-#                     features.append(digitLen)
-#                     # digit rate in title
-#                     features.append(digitLen * 1. / titleLen)
-#                     # letter num in title
-#                     features.append(letterLen)
-#                     # letter rate in title
-#                     features.append(letterLen * 1. / titleLen)
-#                     # space num in title
-#                     features.append(spaceLen)
-#                     # space rate in title
-#                     features.append(spaceLen * 1. / titleLen)
-#                     # booktitle flag in title
-#                     features.append(booktitleFlag)
-#                 # description length
-#                 features.append(len(videoMap[vid][2]))
-#                 # duration
-#                 features.append(videoMap[vid][3])
-#                 # category
-#                 features.append(videoMap[vid][4])
-#                 # public_type
-#                 features.append(videoMap[vid][5])
-#                 # tag num
-#                 features.append(len(videoMap[vid][6].split(',')))
-#                 # copyright_type
-#                 features.append(videoMap[vid][7])
-#                 # streamtype num
-#                 features.append(len(videoMap[vid][8]))
-#                 # whether streamtypes contains hd2
-#                 features.append('hd2' in videoMap[vid][8])
-#                 
-#                 #0:name, gender, description, videos_count, playlists_count
-#                 #favorites_count, followers_count, following_count, statuses_count, subscribe_count
-#                 #vv_count, is_vip, is_share, is_verified, regist_time
-#                 # user name length
-#                 userLen = len(userMap[uid][0])
-#                 features.append(userLen)
-#                 userCncharLen = len(cncharPart.findall(userMap[uid][0]))
-#                 userDigitLen = len(digitPart.findall(userMap[uid][0]))
-#                 userLetterLen = len(letterPart.findall(userMap[uid][0]))
-#                 # cn char num in user name
-#                 features.append(userCncharLen)
-#                 # cn char rate in user name
-#                 features.append(userCncharLen * 1. / userLen)
-#                 # non cn char num in user name
-#                 features.append(userLen - userCncharLen)
-#                 # non cn char rate in user name
-#                 features.append((userLen - userCncharLen) * 1. / userLen)
-#                 # digit num in user name
-#                 features.append(userDigitLen)
-#                 # digit rate in user name
-#                 features.append(userDigitLen * 1. / userLen)
-#                 # letter num in user name
-#                 features.append(userLetterLen)
-#                 # letter rate in user name
-#                 features.append(userLetterLen * 1. / userLen)
-#                 # gender
-#                 features.append(userMap[uid][1])
-#                 # length of user description
-#                 features.append(0 if isinstance(userMap[uid][2], NoneType) else len(userMap[uid][2]))
-#                 # videos_count
-#                 features.append(userMap[uid][3])
-#                 # playlists_count
-#                 features.append(userMap[uid][4])
-#                 # favorites_count
-#                 features.append(userMap[uid][5])
-#                 # followers_count
-#                 features.append(userMap[uid][6])
-#                 # following_count
-#                 features.append(userMap[uid][7])
-#                 # statuses_count
-#                 features.append(userMap[uid][8])
-#                 # subscribe_count
-#                 features.append(userMap[uid][9])
-#                 # vv_count
-#                 features.append(userMap[uid][10])
-#                 # is_vip
-#                 features.append(userMap[uid][11])
-#                 # is_share
-#                 features.append(False if 0 == userMap[uid][12] else True)
-#                 # is_verified
-#                 features.append(False if 0 == userMap[uid][13] else True)
-#                 # i1 - i_indidate
-#                 for i in vciList[0 : 0 + indidate]:
-#                     features.append(i)
-#                 # n_indidate
-#                 s = sum(vciList[0 : 0 + indidate])
-#                 features.append(s)
-#                 # i1 - i_indidate pct
-#                 for i in vciList[0 : 0 + indidate]:
-#                     if 0 == s:
-#                         features.append(i)
-#                     else:
-#                         features.append(i * 1. / s)
-#                 
-#                 if True == details:
-#                     # print features
-#                     outFd.write(vid + '\t' + str(sum(vciList)) + '\t' + (str(True) if '1' == label else str(False)) + '\n')
-#                     for f in videoMap[vid]:
-#                         outFd.write((f.encode('UTF-8') if isinstance(f, UnicodeType) else str(f)) + '\t')
-#                     outFd.write('\n')
-#                     for f in userMap[uid]:
-#                         outFd.write((f.encode('UTF-8') if isinstance(f, UnicodeType) else str(f)) + '\t')
-#                     outFd.write('\n')
-#                     for f in features:
-#                         outFd.write((f.encode('UTF-8') if isinstance(f, UnicodeType) else str(f)) + '\t')
-#                     outFd.write('\n\n')                
-#                 else:
-#                 # output
-#                     outFd.write(vid + '\t' + str(sum(vciList)) + '\t' + (str(True) if '1' == label else str(False)))
-#                     for f in features:
-#                         outFd.write('\t' + (f.encode('unicode-escape') if isinstance(f, UnicodeType) else str(f)))
-#                     outFd.write('\n')
-#                 
-#             else:
-#                 continue
-#         else:
-#             continue
-#         
-#         #break
-#     labelFd.close()
-#     outFd.close()
-
-
-
 
 
 
@@ -911,6 +782,17 @@ if '__main__' == __name__:
 #         get_user_info(datapath + 'user_detail/', 
 #                       d, 
 #                       workpath + 'features/user statistic/' + d)
+
+
+
+#     # get historical popularitys
+#     for d in date_strs:
+#         get_historical_populairty(workpath + 'data/view count clean increase/' + d, 
+#                                   datapath + 'video_detail/', 
+#                                   d, 
+#                                   workpath + 'features/historical popularity/' + d)
+
+
 
     print('All Done!')
 
